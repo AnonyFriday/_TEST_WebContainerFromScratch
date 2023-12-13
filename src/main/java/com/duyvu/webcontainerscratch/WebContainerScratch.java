4cd7ce9215bbb30e746333d56cce0c00025c347a
AnonyFriday <duyvukim752000@gmail.com>
2023-12-13 21:01:45 +0700

    First Commit

---
+++ src/main/java/com/duyvu/webcontainerscratch/WebContainerScratch.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.duyvu.webcontainerscratch;
+
+/**
+ *
+ * @author duyvu
+ */
+public class WebContainerScratch {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
