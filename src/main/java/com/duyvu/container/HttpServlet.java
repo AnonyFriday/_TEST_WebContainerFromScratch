2b4f8b9cf9b822db29b3d361da08e4a0ee22ae21
AnonyFriday <duyvukim752000@gmail.com>
2023-12-13 23:20:17 +0700

    Add the simple HttpServlet as the one from jakarta ee

---
+++ src/main/java/com/duyvu/container/HttpServlet.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.duyvu.container;
+
+/**
+ * A mimic version of the HttpServlet from the Servlet architecture
+ *
+ * @author duyvu
+ */
+public class HttpServlet {
+
+    // =================================
+    // == HttpServlet Lifecycle
+    // =================================
+    public void init() {
+        System.out.println("HttpServlet Init Default Impl...");
+    }
+
+    public void service() {
+        // TODO: Request, Response Object as Parameters
+    }
+
+    public void destroy() {
+
+    }
+
+    public void doGet() {
+
+    }
+
+    public void doPost() {
+
+    }
+}
