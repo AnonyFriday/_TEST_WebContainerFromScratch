13b6f55cea04f7622f27e3af5fc17ab94b47d66a
AnonyFriday <duyvukim752000@gmail.com>
2023-12-14 17:43:53 +0700

    Add loader for config files

--- src/main/java/com/duyvu/container/SimpleWebContainer.java
+++ src/main/java/com/duyvu/container/SimpleWebContainer.java
@@ -13,19 +13,31 @@ import java.net.Socket;
 import java.time.LocalDateTime;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.io.InputStream;
+import java.util.Properties;
 
 /**
- *
  * @author duyvu
  */
 public class SimpleWebContainer {
 
+    // =================================
+    // == Fields
+    // =================================
     private final int port;
+    private final String configFileName;
 
-    public SimpleWebContainer(int port) {
+    // =================================
+    // == Constructors
+    // =================================
+    public SimpleWebContainer(int port, String configFileName) {
         this.port = port;
+        this.configFileName = configFileName;
     }
 
+    // =================================
+    // == Methods
+    // =================================
     /**
      * Starting a server
      */
@@ -46,9 +58,42 @@ public class SimpleWebContainer {
         }
     }
 
+    /**
+     * Load the config.propeties file as the url mapping to the servlet
+     * https://viettuts.vn/vi-du-java-io/read-properties-file-trong-java
+     */
+    public void readPropertiesFile() {
+
+        //Get the inputstream to load the config file into the program
+        InputStream input = getClass()
+                .getClassLoader()
+                .getResourceAsStream(configFileName);
+
+        // Checking if found the config file
+        if (input == null) {
+            throw new RuntimeException("Unable to find file: "
+                    + configFileName);
+        } else {
+            // Reading a config file if reading successfully
+            Properties properties = new Properties();
+            try {
+                properties.load(input);
+            } catch (IOException ex) {
+                ex.getStackTrace();
+            }
+        }
+    }
+
     // Entry point to start a web container
     public static void main(String[] args) {
-        SimpleWebContainer container = new SimpleWebContainer(8080);
+        SimpleWebContainer container
+                = new SimpleWebContainer(8080, ""
+                        + "config.properties");
+
+        // Read url mappings from properties file
+        container.readPropertiesFile();
+
+        // Start the container
         container.start();
     }
 }
