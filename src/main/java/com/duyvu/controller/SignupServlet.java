89af81e63e14538ce8c00da0a33d06eea96f85eb
AnonyFriday <duyvukim752000@gmail.com>
2023-12-16 21:12:35 +0700

    Add doGet() and doPost() method

--- src/main/java/com/duyvu/controller/SignupServlet.java
+++ src/main/java/com/duyvu/controller/SignupServlet.java
@@ -1,6 +1,10 @@
 package com.duyvu.controller;
 
+import com.duyvu.container.HttpRequest;
+import com.duyvu.container.HttpResponse;
 import com.duyvu.container.HttpServlet;
+
+import java.io.PrintWriter;
 
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
@@ -17,5 +21,25 @@ public class SignupServlet extends HttpServlet {
     @Override
     public void init() {
         System.out.println("init() called from signup");
+    }
+
+    @Override
+    public void doGet(HttpRequest request, HttpResponse response) {
+        PrintWriter out = response.getPrintWriter();
+        out.println("<html><body>");
+        out.println("<form method=\"POST\">");
+        out.println("Username: <input type=\"text\" id=\"user\" name=\"user\" value=\"Tina\"><br>");
+        out.println("Password: <input type=\"password\" id=\"password\" name=\"password\" value=\"Xing\"><br><br>");
+        out.println("<input type=\"submit\" value=\"Submit\">");
+        out.println("</body></html>");
+    }
+
+    @Override
+    public void doPost(HttpRequest request, HttpResponse response) {
+        PrintWriter out = response.getPrintWriter();
+        out.println("<html><body>");
+        out.println("<p>Username: " + request.getParamValue("user") + "</p>");
+        out.println("<p>Password: " + request.getParamValue("password") + "</p>");
+        out.println("</body></html>");
     }
 }
